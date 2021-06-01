a6749e0b85338755d834f0dea2a04c3144580b34
ashwani2 <ashwani.kumar1406@gmail.com>
2021-06-01 13:18:52 +0530

    Intial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+
+//All authors route
+
+router.get("/", async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const author = await Author.find(searchOptions)
+        res.render("authors/index", {
+            authors: author,
+            searchOptions: req.query
+        });
+    } catch {
+        res.redirect("/");
+    }
+
+})
+
+//new author
+router.get("/new", (req, res) => {
+    res.render("authors/new", {
+        author: new Author()
+    });
+})
+
+router.post("/", async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors');
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 require("./db/conn");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(expressLayouts);
 app.use(express.static("public"));
 
@@ -19,7 +22,7 @@ app.use(express.static("public"));
 
 
 app.use("/", indexRouter);
-
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server started on the port 3000");
